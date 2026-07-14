4afee0a9101a5d5e797e7c239a716b2163246796
Juankrom17 <krombergerj@gmail.com>
2026-07-14 15:54:16 -0300

    Update Pokefinder.py

--- Pokefinder.py
+++ Pokefinder.py
@@ -362,9 +362,7 @@ class PokemonFinderNLP:
             # 1. Creamos un script VBS (Reemplazo silencioso y limpio del .bat)
           
 
-            # 1. Preparamos las rutas
-            import subprocess
-            import os
+            
 
             # 1. Preparamos las rutas
             vbs_path = os.path.join(os.path.dirname(exe_path), "update.vbs")
